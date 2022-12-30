8b0b1ad19ff93de4c7b2b2c39a52ba8c287fe7bc
fallaym <maks.platon2012@gmail.com>
2022-12-30 03:05:19 +0300

    Creating

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 # 4sensor t+h; rework soon
 dic = {'t1': '', 't2': '', 't3': '', 't4': '', 'h1': '', 'h2': '', 'h3': '', 'h4': '', 't_av': '', 'h_av': ''}
 
+
 # getting temperature+humidity
 def get_t_h():
     ct = 0
@@ -14,21 +15,23 @@ def get_t_h():
         dic[f'h{i}'] = response_t_h['humidity']
         ct += response_t_h['temperature']
         ch += response_t_h['humidity']
-    dic['t_av'] = '%.2f' % (ct/4)
-    dic['h_av'] = '%.2f' % (ch/4)
+    dic['t_av'] = '%.2f' % (ct / 4)
+    dic['h_av'] = '%.2f' % (ch / 4)
     print(dic)
+
+# switching window by patch
+def switch_window():
+    state_value = 0
+    url = 'https://dt.miet.ru/ppo_it/api/fork_drive/'
+    params = {"state": state_value}
+    r = requests.patch(url=url, params=params)
+    print(r)
+    print(r.content)
 
 # prog init
 while True:
     get_t_h()
     time.sleep(30)
-
-# testing patch...
-# state_value = 1
-# url = 'https://dt.miet.ru/ppo_it/api/fork_drive'
-# data = {"state": state_value}
-# r = requests.patch(url=url,data=data)
-# print(r)
 
 
 
