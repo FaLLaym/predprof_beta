a2f20fc55add36a7696af565afba5236b417de8e
0ldcook <timoon2005@gmail.com>
2023-02-07 01:37:58 +0300

    state refactor beta

--- frontend/pages/main.js
+++ frontend/pages/main.js
@@ -2,7 +2,11 @@
 const switchElement = document.getElementById("switch_wid");
 const url = "http://localhost:5000/api/sensor/window/get-state"
 const URL = "http://localhost:5000/api/sensor/window/change-state/"
-var switchState = "close";
+
+const response = await fetch(url);
+var data = await response.json();
+var switchState = data.state;
+console.log(switchState);
 switchElement.addEventListener("click", function() {
 
   if (switchState == "close") {
@@ -31,4 +35,10 @@ switchElement.addEventListener("click", function() {
 
 });
 
-
+///window last opening///
+const url_wlsc = "http://localhost:5000/api/sensor/window/last-state-change/open"
+const response_wlsc = await fetch(url_wlsc);
+var data_wlsc = await response_wlsc.json();
+var lst_open = data_wlsc.date;
+console.log(lst_open);
+last_open.innerText = lst_open;
