0b725c144d1cf408416027fc7143276fc18d7620
0ldcook <timoon2005@gmail.com>
2023-02-14 00:25:35 +0300

    main page and function refactor beta

--- frontend/pages/main.js
+++ frontend/pages/main.js
@@ -1,3 +1,112 @@
+//window
+const switchElement = document.getElementById("switch_wid");
+const switchElement_hum = document.getElementById("switch_hum");
+const url = "http://localhost:5000/api/sensor/window/get-state"
+const url_hum = "http://localhost:5000/api/sensor/total_hum/get-state"
+
+const response = await fetch(url);
+var data = await response.json();
+var switchState = data.state;
+console.log(switchState);
+switchElement.addEventListener("change", function() {
+
+  if (switchState == "close") {
+    switchElement.setAttribute("data-state", "open");
+    switchState = "open";
+  } else {
+    switchElement.setAttribute("data-state", "close");
+    switchState = "close";
+  }
+
+
+  fetch(`http://localhost:5000/api/sensor/window/change-state/${switchState}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log("State posted successfully");
+      } else {
+        console.error("Failed to post state");
+      }
+    })
+    .catch(error => console.error(error));
+
+});
+
+const response_hum = await fetch(url_hum);
+var data_hum = await response_hum.json();
+var switchState_hum = data_hum.state;
+console.log(switchState_hum);
+
+switchElement_hum.addEventListener("change", function() {
+
+  if (switchState_hum == "off") {
+    switchElement_hum.setAttribute("data-state", "on");
+    switchState_hum = "on";
+  } else {
+    switchElement_hum.setAttribute("data-state", "off");
+    switchState_hum = "off";
+  }
+
+
+  fetch(`http://localhost:5000/api/sensor/total_hum/${switchState_hum}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+    .then(response_hum => {
+      if (response_hum.ok) {
+        console.log("State posted successfully");
+      } else {
+        console.error("Failed to post state");
+      }
+    })
+    .catch(error => console.error(error));
+
+});
+
+
+/**window last opening///
+const url_wlsc = "http://localhost:5000/api/sensor/window/last-state-change/open"
+const response_wlsc = await fetch(url_wlsc);
+var data_wlsc = await response_wlsc.json();
+var lst_open = data_wlsc.date;
+console.log(lst_open);
+last_open.innerText = lst_open;
+**/
+
+/**humidity last opening///
+const url_hlsc = "http://localhost:5000/api/sensor/watering/last-state-change/"
+const response_hlsc = await fetch(url_hlsc);
+var data_hlsc = await response_hlsc.json();
+var hum_lst_opening = data_hlsc.date;
+console.log(hum_lst_opening);
+hum_last_open.innerText = hum_lst_opening;
+
+**/
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+fetch("http://localhost:5000/api/temp_hum/get-data?t=5M")
+  .then(response => response.json())
+  .then(data => {
+    const responseData = data;
+    // use responseData as needed here
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+const response_th = await fetch("http://localhost:5000/api/temp_hum/get-data?t=5M");
+var data_th = await response.json();
+var data_th_now = data_th.data;
+console.log(data_th);
+
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
@@ -33,65 +142,3 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-
-
-
-
-
-//window
-const switchElement = document.getElementById("switch_wid");
-const url = "http://localhost:5000/api/sensor/window/get-state"
-const URL = "http://localhost:5000/api/sensor/window/change-state/"
-
-const response = await fetch(url);
-var data = await response.json();
-var switchState = data.state;
-console.log(switchState);
-switchElement.addEventListener("click", function() {
-
-  if (switchState == "close") {
-    switchElement.setAttribute("data-state", "open");
-    switchState = "open";
-  } else {
-    switchElement.setAttribute("data-state", "close");
-    switchState = "close";
-  }
-
-
-  fetch(`http://localhost:5000/api/sensor/window/change-state/${switchState}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log("State posted successfully");
-      } else {
-        console.error("Failed to post state");
-      }
-    })
-    .catch(error => console.error(error));
-
-});
-
-///window last opening///
-const url_wlsc = "http://localhost:5000/api/sensor/window/last-state-change/open"
-const response_wlsc = await fetch(url_wlsc);
-var data_wlsc = await response_wlsc.json();
-var lst_open = data_wlsc.date;
-console.log(lst_open);
-last_open.innerText = lst_open;
-
-
-///humidity last opening///
-const url_hlsc = "http://localhost:5000/api/sensor/watering/last-state-change/"
-const response_hlsc = await fetch(url_hlsc);
-var data_hlsc = await response_hlsc.json();
-var hum_lst_opening = data_hlsc.date;
-console.log(hum_lst_opening);
-hum_last_open.innerText = hum_lst_opening;
-
-///
-
-
