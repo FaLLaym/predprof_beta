f630c75c0b61165369950dde55729c622ef71dd4
0ldcook <timoon2005@gmail.com>
2023-02-15 20:08:00 +0300

    i'm tired(moon)

--- frontend/pages/js/main.js
+++ frontend/pages/js/main.js
@@ -96,17 +96,19 @@ last_thum_open.innerText = hum_lst_opening;
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
+Chart.defaults.global.legend.display = false;
+
 const response_th = await fetch("http://localhost:5000/api/temp_hum/get-data?t=5M");
 var data_th = await response_th.json();
 var data_th_now = data_th.data;
 console.log(data_th);
-current_data_t.innerText = data_th.data[0][0].split(' ')[1].split('.')[0];
+current_data_t.innerText = data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[0]+':'+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[1]+":"+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[2];
 
 var ctx_t = document.getElementById('Chart_temp').getContext('2d');
 var myChart_temp = new Chart(ctx_t, {
     type: 'bar',
     data: {
-        labels: ['1', '2', '3', '4', 'ср'],
+        labels: ['1', '2', '3', '4', 'avg'],
         datasets: [{
             label: 'temperature',
             data: [data_th.data[0][1], data_th.data[0][2],data_th.data[0][3], data_th.data[0][4], data_th.data[0][9]],
@@ -131,13 +133,14 @@ var myChart_temp = new Chart(ctx_t, {
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-current_data_h.innerText = data_th.data[0][0].split(' ')[1].split('.')[0];
+current_data_h.innerText = data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[0]+':'+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[1]+":"+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[2];
+
 
 var ctx_h = document.getElementById('Chart_hum').getContext('2d');
 var myChart_hum = new Chart(ctx_h, {
     type: 'bar',
     data: {
-        labels: ['1', '2', '3', '4', 'ср'],
+        labels: ['1', '2', '3', '4', 'avg'],
         datasets: [{
             label: 'Humidity',
             data: [data_th.data[0][5], data_th.data[0][6],data_th.data[0][7], data_th.data[0][8], data_th.data[0][10]],
@@ -168,21 +171,25 @@ setInterval(async function() {
   myChart_temp.update();
   myChart_hum.data.datasets[0].data = [data_th.data[0][5], data_th.data[0][6],data_th.data[0][7], data_th.data[0][8], data_th.data[0][10]];
   myChart_hum.update();
-  current_data_t.innerText = data_th.data[0][0].split(' ')[1].split('.')[0];
-  current_data_h.innerText = data_th.data[0][0].split(' ')[1].split('.')[0];
+  let time = data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[0]+':'+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[1]+":"+data_th.data[0][0].split(' ')[1].split('.')[0].split('-')[2];
+  current_data_h.innerText = time;
+  current_data_t.innerText = time;
+
+
 }, 15000);
 ////////////////////////////////////////////////////////////////////////////////////////////////
 const response_hb = await fetch("http://localhost:5000/api/hum/get-data");
 var data_hb = await response_hb.json();
 var data_hb_now = data_hb.data;
 console.log(data_hb);
-current_data_hb.innerText = data_hb.data[0][0].split(' ')[1].split('.')[0];
+current_data_hb.innerText = data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[0]+':'+data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[1]+":"+data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[2];
+
 
 var ctx_hb = document.getElementById('Chart_hb').getContext('2d');
 var myChart_hb = new Chart(ctx_hb, {
     type: 'bar',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6', 'ср'],
+        labels: ['1', '2', '3', '4', '5', '6', 'avg'],
         datasets: [{
             label: 'Soil Humidity',
             data: [data_hb.data[0][1], data_hb.data[0][2], data_hb.data[0][3], data_hb.data[0][4], data_hb.data[0][5], data_hb.data[0][6], data_hb.data[0][7]],
@@ -211,7 +218,7 @@ setInterval(async function() {
   const response_hb = await fetch("http://localhost:5000/api/hum/get-data");
   var data_hb = await response_hb.json();
   var data_hb_now = data_hb.data;
-  current_data_hb.innerText = data_hb.data[0][1].split(' ')[0];
+  current_data_hb.innerText = data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[0]+':'+data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[1]+":"+data_hb.data[0][0].split(' ')[1].split('.')[0].split('-')[2];
 
   myChart_hb.data.datasets[0].data = [data_hb.data[0][1], data_hb.data[0][2], data_hb.data[0][3], data_hb.data[0][4], data_hb.data[0][5], data_hb.data[0][6], data_hb.data[0][7]];
   myChart_hb.update();
