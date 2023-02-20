bb464134b6a7763e5b63098486894c6eab8e4838
ZGuyDev <77798764+ZGuyDev@users.noreply.github.com>
2023-02-20 14:04:58 +0300

    Update api_parser.py

--- backend/api_parser.py
+++ backend/api_parser.py
@@ -1,15 +1,11 @@
 from .handlers import sensors_pool
 from .handlers.config_handler import db_update_frequency_per_minute
 
-from time import time as clock, sleep as wait
+from time import sleep as wait
 
 def main() -> None:
     while 1:
-            start = clock()
-
             sensors_pool.update_temp_hum()
             sensors_pool.update_hum()
 
-            delta = clock() - start
-            if delta < 60 / db_update_frequency_per_minute:
-                wait(delta)
+            wait(60 / db_update_frequency_per_minute)
