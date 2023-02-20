72459542d921667317c38795fbbee56d456cdbbe
fallaym <maks.platon2012@gmail.com>
2023-02-20 13:33:02 +0300

    timofey

--- frontend/pages/js/main.js
+++ frontend/pages/js/main.js
@@ -127,7 +127,7 @@ setInterval(async function() {
   current_data_h.innerText = time;
   current_data_t.innerText = time;
   IsAbleHT(data_th_now)
-}, 15000);
+}, 10000);
 ////////////////////////////////////////////////////////////////////////////////////////////////
 const response_hb = await fetch("http://localhost:5000/api/hum/get-data");
 var data_hb = await response_hb.json();
@@ -172,7 +172,7 @@ setInterval(async function() {
 
   myChart_hb.data.datasets[0].data = [data_hb.data[0][1], data_hb.data[0][2], data_hb.data[0][3], data_hb.data[0][4], data_hb.data[0][5], data_hb.data[0][6], data_hb.data[0][7]];
   myChart_hb.update();
-}, 150000);
+}, 1000);
 
 
 
@@ -301,58 +301,4 @@ switchElement_hum.addEventListener("change", function() {
     .catch(error => console.error(error));
   }
 
-});
-describe('IsAbleHT', () => {
-  it('should disable humidity toggle when humidity is too high', () => {
-    // Arrange
-    const data_th_now = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90]]; // humidity is 90%
-    const toggle_hum = { disabled: false, checked: false };
-    const toggle_wid = { disabled: false, checked: false };
-    sessionStorage.setItem('mode', 'false');
-    sessionStorage.setItem('H', '80');
-
-    // Act
-    IsAbleHT(data_th_now);
-
-    // Assert
-    expect(toggle_hum.disabled).toBe(true);
-    expect(toggle_hum.checked).toBe(false);
-    expect(toggle_wid.disabled).toBe(false);
-    expect(toggle_wid.checked).toBe(false);
-  });
-
-  it('should disable wind toggle when temperature is too low', () => {
-    // Arrange
-    const data_th_now = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90]];
-    const toggle_hum = { disabled: false, checked: false };
-    const toggle_wid = { disabled: false, checked: false };
-    sessionStorage.setItem('mode', 'false');
-    sessionStorage.setItem('T', '10');
-
-    // Act
-    IsAbleHT(data_th_now);
-
-    // Assert
-    expect(toggle_wid.disabled).toBe(true);
-    expect(toggle_wid.checked).toBe(false);
-    expect(toggle_hum.disabled).toBe(false);
-    expect(toggle_hum.checked).toBe(false);
-  });
-
-  it('should not disable any toggles in manual mode', () => {
-    // Arrange
-    const data_th_now = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90]];
-    const toggle_hum = { disabled: false, checked: false };
-    const toggle_wid = { disabled: false, checked: false };
-    sessionStorage.setItem('mode', 'true');
-
-    // Act
-    IsAbleHT(data_th_now);
-
-    // Assert
-    expect(toggle_hum.disabled).toBe(false);
-    expect(toggle_hum.checked).toBe(false);
-    expect(toggle_wid.disabled).toBe(false);
-    expect(toggle_wid.checked).toBe(false);
-  });
 });
